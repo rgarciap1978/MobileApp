06ca47f18ff20b1f6d2fa918ba7c4410f8bbdeeb
Roberto Garcia <rogarpez@hotmail.com>
2023-11-03 18:55:25 +1100

    Init commit

---
+++ app/src/main/java/com/example/mobileapp/AuthActivity.kt
@@ -0,0 +1,226 @@
+package com.example.mobileapp
+
+import android.accounts.AuthenticatorException
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.facebook.CallbackManager
+import com.facebook.FacebookCallback
+import com.facebook.FacebookException
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
+import kotlinx.android.synthetic.main.activity_auth.*
+
+class AuthActivity : AppCompatActivity() {
+
+    private val GOOGLE_SIGN_IN = 100
+    private val callbackManager = CallbackManager.Factory.create()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        // Splash
+        Thread.sleep(2000)
+        setTheme(R.style.AppTheme)
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+
+        // Analytics Event
+        val analytics = FirebaseAnalytics.getInstance(this)
+        val bundle = Bundle()
+        bundle.putString("message", "Integración de Firebase completa")
+        analytics.logEvent("InitScreen", bundle)
+
+        // Setup
+        notifiation()
+        setup()
+        session()
+    }
+
+    private fun notifiation() {
+        FirebaseInstanceId.getInstance().instanceId.addOnCompleteListener {
+            it.result?.token?.let {
+                println("Este es el token del dispositivo: ${it}")
+            }
+        }
+
+        // Temas (Topics)
+        FirebaseMessaging.getInstance().subscribeToTopic("tutorial")
+
+        // Recuperar información desde una notificación
+        val url = intent.getStringExtra("url")
+        url?.let {
+            println("Ha llegado información en una push: ${it}")
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        authLayout.visibility = View.VISIBLE
+    }
+
+    private fun session() {
+
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val email = prefs.getString("email", null)
+        val provider = prefs.getString("provider", null)
+
+        if (email != null && provider != null) {
+            authLayout.visibility = View.INVISIBLE
+            showHome(email, ProviderType.valueOf(provider))
+        }
+    }
+
+    private fun setup() {
+
+        title = "Autenticación"
+
+        signUptButton.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
+
+                FirebaseAuth
+                    .getInstance()
+                    .createUserWithEmailAndPassword(
+                        emailEditText.text.toString(),
+                        passwordEditText.text.toString()
+                    ).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                        } else {
+                            showAlert()
+                        }
+                    }
+            }
+        }
+
+        loginButton.setOnClickListener {
+            if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
+
+                FirebaseAuth
+                    .getInstance()
+                    .signInWithEmailAndPassword(
+                        emailEditText.text.toString(),
+                        passwordEditText.text.toString()
+                    ).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                        } else {
+                            showAlert()
+                        }
+                    }
+            }
+        }
+
+        googleButton.setOnClickListener {
+            val googleConf = GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+            val googleClient = GoogleSignIn.getClient(this, googleConf)
+            googleClient.signOut()
+
+            startActivityForResult(googleClient.signInIntent, GOOGLE_SIGN_IN)
+        }
+
+        facebookButton.setOnClickListener {
+
+            //LoginManager.getInstance().logInWithPublishPermissions(this, listOf("email"))
+            LoginManager.getInstance().logInWithReadPermissions(this, listOf("email"))
+
+            LoginManager.getInstance().registerCallback(callbackManager,
+                object : FacebookCallback<LoginResult> {
+                    override fun onSuccess(result: LoginResult?) {
+                        result?.let {
+                            val token = it.accessToken
+
+                            val credencial = FacebookAuthProvider.getCredential(token.token)
+
+                            FirebaseAuth.getInstance().signInWithCredential(credencial)
+                                .addOnCompleteListener {
+                                    if (it.isSuccessful) {
+                                        showHome(
+                                            it.result?.user?.email ?: "",
+                                            ProviderType.FACEBOOK
+                                        )
+                                    } else {
+                                        showAlert()
+                                    }
+                                }
+
+                        }
+                    }
+
+                    override fun onCancel() {
+
+                    }
+
+                    override fun onError(error: FacebookException?) {
+                        showAlert()
+                    }
+
+                })
+        }
+    }
+
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Se ha producido un error autenticando al usuario")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+
+    private fun showHome(email: String, provider: ProviderType) {
+        val homeIntent = Intent(this, HomeActivity::class.java).apply {
+            putExtra("email", email)
+            putExtra("provider", provider.name)
+        }
+
+        startActivity(homeIntent)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GOOGLE_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            try {
+                val account = task.getResult(ApiException::class.java)
+
+                if (account != null) {
+                    val credencial = GoogleAuthProvider.getCredential(account.idToken, null)
+                    FirebaseAuth.getInstance().signInWithCredential(credencial)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                showHome(account.email ?: "", ProviderType.GOOGLE)
+                            } else {
+                                showAlert()
+                            }
+                        }
+                }
+            } catch (e: ApiException) {
+                showAlert()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mobileapp/HomeActivity.kt
@@ -0,0 +1,57 @@
+package com.example.mobileapp
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.facebook.login.LoginManager
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_home.*
+
+enum class ProviderType {
+    BASIC,
+    GOOGLE,
+    FACEBOOK
+}
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        // Setup
+        val bundle: Bundle? = intent.extras
+        val email: String? = bundle?.getString("email")
+        val provider: String? = bundle?.getString("provider")
+        setup(email ?: "", provider ?: "")
+
+        // Save data
+        val prefs =
+            getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.putString("email", email)
+        prefs.putString("provider", provider)
+        prefs.apply()
+    }
+
+    private fun setup(email: String, provider: String) {
+
+        title = "Inicio"
+        emailTextView.text = email
+        providerTextView.text = provider
+
+        logOutButton.setOnClickListener {
+            // Delete data
+            val prefs =
+                getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+            prefs.clear()
+            prefs.apply()
+
+            if (provider == ProviderType.FACEBOOK.name) {
+                LoginManager.getInstance().logOut()
+            }
+
+            FirebaseAuth.getInstance().signOut()
+            onBackPressed()
+        }
+    }
+}
